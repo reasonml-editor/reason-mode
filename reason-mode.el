f983db17748c931239ebced141215da0b067126a
Fredrik Dyrkell <fredrik.dyrkell@gmail.com>
2017-05-19 19:24:14 +0200

    Added back accidental removal of commentary

--- reason-mode.el
+++ reason-mode.el
@@ -12,6 +12,21 @@
 ;; This file is distributed under the terms of both the MIT license and the
 ;; Apache License (version 2.0).
 
+;;; Commentary:
+;; This project provides useful functions and helpers for developing code
+;; using the Reason programming language (https://facebook.github.io/reason).
+;;
+;; Reason is an umbrella project that provides a curated layer for OCaml.
+;;
+;; It offers:
+;;  - A new, familiar syntax for the battle-tested language that is OCaml.
+;;  - A workflow for compiling to JavaScript and native code.
+;;  - A set of friendly documentations, libraries and utilities.
+;;
+;; See the README.md for more details.
+
+;;; Code:
+
 (eval-when-compile (require 'rx)
                    (require 'compile)
                    (require 'url-vars))
